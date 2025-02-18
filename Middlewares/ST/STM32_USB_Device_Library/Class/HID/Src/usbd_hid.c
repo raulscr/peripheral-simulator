80f0b17e186896a64f71608ed873d2c334d740e1
Raul Scarmocin <raulsfreitas1997@hotmail.com>
2025-02-17 23:34:31 -0300

    fix: firmware support for multi-device descriptor is now properly working

--- Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c
+++ Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c
@@ -135,10 +135,10 @@ USBD_ClassTypeDef  USBD_HID =
 
 __ALIGN_BEGIN static uint8_t HID_ReportDesc[]  __ALIGN_END =
 {
-
 	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
 	0x09, 0x06,                    // USAGE (Keyboard)
 	0xa1, 0x01,                    // COLLECTION (Application)
+	0x85, 0x01,                    // DEVICE ID
 	0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
 	0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
 	0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
@@ -167,45 +167,37 @@ __ALIGN_BEGIN static uint8_t HID_ReportDesc[]  __ALIGN_END =
 	0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
 	0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
 	0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-	0xc0,                           // END_COLLECTION
-	// Mouse
-//  0x05,   0x01,
-//  0x09,   0x02,
-//  0xA1,   0x01,
-//  0x09,   0x01,
-//  0xA1,   0x00,
-//  0x05,   0x09,
-//  0x19,   0x01,
-//  0x29,   0x03,
-//  0x15,   0x00,
-//  0x25,   0x01,
-//  0x95,   0x03,
-//  0x75,   0x01,
-//  0x81,   0x02,
-//  0x95,   0x01,
-//  0x75,   0x05,
-//  0x81,   0x01,
-//  0x05,   0x01,
-//  0x09,   0x30,
-//  0x09,   0x31,
-//  0x09,   0x38,
-//  0x15,   0x81,
-//  0x25,   0x7F,
-//  0x75,   0x08,
-//  0x95,   0x03,
-//  0x81,   0x06,
-//  0xC0,   0x09,
-//  0x3c,   0x05,
-//  0xff,   0x09,
-//  0x01,   0x15,
-//  0x00,   0x25,
-//  0x01,   0x75,
-//  0x01,   0x95,
-//  0x02,   0xb1,
-//  0x22,   0x75,
-//  0x06,   0x95,
-//  0x01,   0xb1,
-//  0x01,   0xc0
+	0xc0,                          // END_COLLECTION
+
+	// **Mouse**
+	0x05, 0x01,        // USAGE_PAGE (Generic Desktop)
+	0x09, 0x02,        // USAGE (Mouse)
+	0xA1, 0x01,        // COLLECTION (Application)
+	0x85, 0x02,        //   REPORT_ID (2)   <-- ID do relatório do mouse
+	0x09, 0x01,        //   USAGE (Pointer)
+	0xA1, 0x00,        //   COLLECTION (Physical)
+	0x05, 0x09,        //     USAGE_PAGE (Button)
+	0x19, 0x01,        //     USAGE_MINIMUM (Button 1)
+	0x29, 0x03,        //     USAGE_MAXIMUM (Button 3)
+	0x15, 0x00,        //     LOGICAL_MINIMUM (0)
+	0x25, 0x01,        //     LOGICAL_MAXIMUM (1)
+	0x95, 0x03,        //     REPORT_COUNT (3)
+	0x75, 0x01,        //     REPORT_SIZE (1)
+	0x81, 0x02,        //     INPUT (Data,Var,Abs)
+	0x95, 0x01,        //     REPORT_COUNT (1)
+	0x75, 0x05,        //     REPORT_SIZE (5)
+	0x81, 0x01,        //     INPUT (Cnst,Var,Abs)
+	0x05, 0x01,        //     USAGE_PAGE (Generic Desktop)
+	0x09, 0x30,        //     USAGE (X)
+	0x09, 0x31,        //     USAGE (Y)
+	0x09, 0x38,        //     USAGE (Wheel)  <-- Adiciona suporte para rolagem do mouse
+	0x15, 0x81,        //     LOGICAL_MINIMUM (-127)
+	0x25, 0x7F,        //     LOGICAL_MAXIMUM (127)
+	0x75, 0x08,        //     REPORT_SIZE (8)
+	0x95, 0x03,        //     REPORT_COUNT (3)  <-- Agora inclui o eixo de rolagem
+	0x81, 0x06,        //     INPUT (Data,Var,Rel)
+	0xC0,              //   END_COLLECTION
+	0xC0,               // END_COLLECTION
 };
 
 const static uint8_t HID_REPORT_DESC_SIZE = sizeof(HID_ReportDesc);
